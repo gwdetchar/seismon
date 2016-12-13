01be711c0076b06d5accf4ba101c054683626b16
Michael Coughlin <michael.coughlin@ligo.org>
2016-12-13 06:33:01 -0800

    Add lock segments

---
+++ segments/get_locks.sh
@@ -0,0 +1,5 @@
+
+ligolw_segment_query_dqsegdb --segment-url=https://segments.ligo.org --query-segments --include-segments H1:ODC-MASTER_GRD_IFO_LOCKED:1 --gps-start-time 1126051217 --gps-end-time 1165674425 | ligolw_print -t segment:table -c start_time -c end_time -d " " > H1-LOCK.txt
+
+ligolw_segment_query_dqsegdb --segment-url=https://segments.ligo.org --query-segments --include-segments L1:ODC-MASTER_GRD_IFO_LOCKED:1 --gps-start-time 1126051217 --gps-end-time 1165674425 | ligolw_print -t segment:table -c start_time -c end_time -d " " > L1-LOCK.txt
+
