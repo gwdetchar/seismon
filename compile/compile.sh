63d9e7beb30c732aaa84b4fa52eff253a1781085
Michael Coughlin <michael.coughlin@ligo-seismon.mit.edu>
2017-03-05 09:43:29 -0500

    Fix script for MIT

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# MATLAB_ROOT must be set differently for atlas than for Caltech.
+h=`hostname -d`
+
+if [[ $h =~ "atlas" ]]; then
+    MATLAB_ROOT="/opt/matlab/2016a"
+elif [[ $h =~ "caltech" ]] || [[ $h =~ "cit" ]]; then
+    MATLAB_ROOT="/ldcg/matlab_r2016a"
+    eval `/ligotools/bin/use_ligotools`
+elif [[ $h =~ "MIT" ]]; then
+    MATLAB_ROOT="/usr/local/MATLAB/R2016b"
+fi
+
 rm 0
 rm *.txt *.log *.ctf
 rm run_seismon_lockloss_prediction.sh
 rm seismon_lockloss_prediction
 
-/ldcg/matlab_r2016a/bin/matlab <<EOF
+${MATLAB_ROOT}/bin/matlab <<EOF
  mcc -R -nojvm -R -nodisplay -R -singleCompThread -m seismon_lockloss_prediction
 EOF
 
@@ -13,8 +25,8 @@ rm *.txt *.log *.ctf
 rm run_seismon_lockloss_prediction.sh
 rm -rf seismon_lockloss_prediction_mcr/
 
-source ${HOME}/matlab_scripts/matlab_script_2016a.sh
+source matlab_script.sh
 ./seismon_lockloss_prediction 0 0 0 0 0 0 0
 
 rm 0
-
+mv seismon_lockloss_prediction ../bin/
