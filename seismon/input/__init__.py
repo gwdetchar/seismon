6a767e7e8d555c5db30f70f14302f7bf8225839e
Michael Coughlin <coughlin@physics.harvard.edu>
2018-06-29 12:46:34 -0700

    Add __init__

---
+++ seismon/input/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) Michael Coughlin (2013)
+#
+# This file is part of SeisMon
+#
+# SeisMon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SeisMon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SeisMon.  If not, see <http://www.gnu.org/licenses/>
+
+"""Seismic activity Monitor
+
+SeisMon is used to monitor and predict the impact of earthquakes on the
+LIGO gravitational-wave detectors.
+"""
+
+__author__ = 'Michael Coughlin <michael.coughlin@ligo.org>'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
