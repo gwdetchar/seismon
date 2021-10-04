ead75d0cddc12bfcf8d9b6d6c2937a57bdacd809
nikhil.mukund <nikhil.mukund@LIGO.ORG>
2021-10-04 16:29:41 -0700

    changes made at pcdev2

--- seismon/tests/test_remote_access_load_from_carleton_db.py
+++ seismon/tests/test_remote_access_load_from_carleton_db.py
@@ -4,7 +4,7 @@ from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine  
 import pandas as pd
 
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_rows', None)
 
 server = SSHTunnelForwarder(
     ('virgo.physics.carleton.edu', 22),
@@ -30,7 +30,7 @@ print(earthquakes_db)
 print('Printing ifos_db')
 print(ifos_db)
 print('Printing seismon ml predictions')
-print(predictions_db.loc[:,['event_id', 'ifo', 'rfamp', 'lockloss']])
-print('Printing public.llo_catalogues')
-print(llo_processed_catalogue_db)
+print(predictions_db.loc[:,['event_id', 'ifo', 'rfamp','rfamp_measured', 'lockloss']])
+print('Printing public.lho_catalogues')
+print(lho_processed_catalogue_db)
 
