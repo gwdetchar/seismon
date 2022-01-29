ea7466d68a9776c504a7d59ca518023a9dc22b65
nikhil.mukund <nikhil.mukund@LIGO.ORG>
2022-01-29 02:57:01 -0800

    update comparisons with measurements

--- seismon/tests/test_remote_access_upload_new_seimometer_event_to_carleton_db.py
+++ seismon/tests/test_remote_access_upload_new_seimometer_event_to_carleton_db.py
@@ -7,6 +7,8 @@ import pandas as pd
 from argparse import ArgumentParser
 import datetime
 import numpy
+import numpy as np
+
 
 pd.set_option('display.max_rows',100)
 
@@ -157,3 +159,31 @@ pd.set_option('display.max_rows', None)
 print(predictions_db.loc[jiD,['event_id', 'ifo', 'rfamp','rfamp_measured', 'lockloss']])
 # close connection
 conn.close()
+
+
+########################################################
+## Compare Predictions vs Measurements (both LLO & LHO combined)
+############################
+print('Comparing Predictions vs Measurements')
+# get predictions &  measurements
+pred=predictions_db.loc[:,['rfamp','rfamp_measured']]
+# pandas to scatter plot
+ax = pred.loc[pred['rfamp_measured']!=-1,:].plot.scatter(x='rfamp_measured',y='rfamp')
+ax.grid(color='r', linestyle='-', linewidth=0.3)
+#ax.set_aspect ('equal', adjustable='box')
+#ax.set_aspect('equal', adjustable='datalim')
+# Add x=y line
+ax.plot([0,1],[0,1], transform=ax.transAxes,linestyle='--', color='k', lw=1.5)
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_aspect('equal')
+minimum = np.min((ax.get_xlim(),ax.get_ylim()))
+maximum = np.max((ax.get_xlim(),ax.get_ylim()))
+ax.set_xlim(minimum*1.2,maximum*1.2)
+ax.set_ylim(minimum*1.2,maximum*1.2)
+ax.margins(0.1)
+# get figure
+fig = ax.get_figure()
+# save fig
+fig.savefig('/home/nikhil.mukund/public_html/SEISMON/compare_predictions_with_measured.png')
+print('Plot saved to: compare_predictions_with_measured.png')
